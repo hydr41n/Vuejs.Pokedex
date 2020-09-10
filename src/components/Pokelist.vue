498cfc8f346f0a6ddc8d4555a5d6ec7aca0ddf52
hydr41n <hydr41n@gmail.com>
2020-09-10 14:40:17 +0800

    first commit

---
+++ src/components/Pokelist.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="card">
+  	<h1>ds</h1>
+  </div>
+</template>
+
+
+<script>
+export default {
+  name: 'Pokelist'
+}
+</script>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,27 @@
 import Vue from 'vue'
 import App from './App.vue'
+const axios = require('axios').default;
 
 Vue.config.productionTip = false
 
 new Vue({
   render: h => h(App),
+  data () {
+    return {
+      pokemonlist: null
+    }
+  },
+  mounted () {
+    axios
+      .get('https://pokeapi.co/api/v2/pokemon/', {
+        params: {
+          offset: 0,
+          limit: 20
+        }
+      })
+      .then(res => (console.log(res.data.results)))
+      .catch(function (error) {
+        console.log(error);
+      })
+  }
 }).$mount('#app')
